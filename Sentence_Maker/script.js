180b4aff93dde7364f5994535d9596812b34e9e7
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2026-01-31 09:52:55 +0200

    Sentence Maker

---
+++ Sentence_Maker/script.js
@@ -0,0 +1,23 @@
+// Declare variables
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "castle";
+let adjective2 = "fiery";
+let noun2 = "cookies";
+
+// First story
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+// Reassign variables for second story
+adjective = "silly";
+noun = "unicorn";
+verb = "dancing";
+place = "forest";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+
+// Second story
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
