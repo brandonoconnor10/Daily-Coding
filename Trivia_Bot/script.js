e5d232a755dd3c1a74389007836c24375ebb1d97
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2026-01-31 09:51:22 +0200

    Trivia Bot

---
+++ Trivia_Bot/script.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!")
+let botName = "TriviaBot"
+let botLocation = "Earth"
+let favoriteLanguage = "JavaScript"
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+let codingFact = "JavaScript is a pain to learn"
+console.log(codingFact)
+codingFact = "Another fact about is " + favoriteLanguage + " is...";
+console.log(codingFact)
+codingFact = "Another fact about is " + favoriteLanguage + " is idk...";
+console.log(codingFact)
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
